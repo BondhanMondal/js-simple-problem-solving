4937a0b7d89b680ddb8b0b4b89e6212288110227
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-31 18:27:53 +0600

    celcious to farenheight added

--- practice.js
+++ practice.js
@@ -6,3 +6,13 @@ function farenheightToCelcious(farenheight){
 }
 const myCelcious = farenheightToCelcious(300);
 console.log('my celcious : ', myCelcious);
+
+//2. Celcious to Farenheight
+
+function celciousToFarenheight(celcious){
+    let farenheight = (celcious * (9 / 5)) + 32;
+    return farenheight; 
+}
+
+const myFarenheight = celciousToFarenheight(500);
+console.log('my farenheight: ', myFarenheight);
